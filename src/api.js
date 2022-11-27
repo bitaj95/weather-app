978d12e6a1e3fd720ca07c3d62f588e750c37915
Bita <bita.janzadeh@hotmail.com>
2022-11-26 23:35:40 -0500

    create file to store api key, host and url

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "83af14037cmsh05d71cf97bc3659p173db1jsn53eb82def901",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";

67f4d3dc82b0bb6c028064aab511456e1c0c7b26
Bita <bita.janzadeh@hotmail.com>
2022-11-30 23:43:59 -0500

    Add styling to page for current weather

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./current-weather.css";
 
 function CurrentWeather() {
-  return <div>CurrentWeather</div>;
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">Tehran</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+      </div>
+    </div>
+  );
 }
 
 export default CurrentWeather;

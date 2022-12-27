8759e2fa3566eca130d7a3b1c20b2f57363c80cc
Bita <bita.janzadeh@hotmail.com>
2022-12-27 15:59:33 -0500

    complete Promise.all with catch, and store res in hooks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Search from "./components/search/search";
@@ -5,6 +6,10 @@ import CurrentWeather from "./components/current-weather/current-weather";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
+  //create hooks to store values from fetch
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setCurrentForecast] = useState(null);
+
   const handleSearchChange = (searchData) => {
     //store location values in corresponding variables
     const [latitude, longitude] = searchData.value.split(" ");
@@ -24,11 +29,18 @@ function App() {
       `${WEATHER_API_URL}/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
     );
 
-    Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
-      const weatherResponse = await response[0].json();
-      const forecastResponse = await response[1].json();
-    });
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setCurrentForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
   };
+
+  console.log(currentWeather, forecast);
 
   return (
     <div className="container">

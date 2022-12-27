4a5be7fcc81b7025c220caafed3d2b98b5933a31
Bita <bita.janzadeh@hotmail.com>
2022-12-27 15:16:08 -0500

    store location values in corresponding variables

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import CurrentWeather from "./components/current-weather/current-weather";
 function App() {
   const handleSearchChange = (searchData) => {
     console.log(searchData);
+    //store location values in corresponding variables
+    const [latitude, longitude] = searchData.value.split(" ");
   };
 
   return (

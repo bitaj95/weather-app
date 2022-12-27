3e3026a65eddb1433f74489ebf6feb5db8f9105c
Bita <bita.janzadeh@hotmail.com>
2022-12-27 15:36:54 -0500

    fix typo, import and use weather api variables for api call in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import logo from "./logo.svg";
 import "./App.css";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
   const handleSearchChange = (searchData) => {
     console.log(searchData);
     //store location values in corresponding variables
     const [latitude, longitude] = searchData.value.split(" ");
+
+    /*   fetching two API calls.
+    1. current weather
+    2. forecast
+    Promise all to fetch both of these.
+    Two fetches, store in variables
+    */
+
+    const currentWeather = fetch(
+      `${WEATHER_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+    );
   };
 
   return (
--- src/api.js
+++ src/api.js
@@ -9,5 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = 'f88f11a1a4f0258e091c2af960fd92fd"
-
+export const WEATHER_API_KEY = "f88f11a1a4f0258e091c2af960fd92fd";
